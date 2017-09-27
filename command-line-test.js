3c48af06c64080a5343c5d7faecece51c7cf6225
Sean Scanlan <sean.scanlan@kettlenyc.com>
2017-09-27 10:08:27 -0400

    Updating command line test

--- command-line-test.js
+++ command-line-test.js
@@ -1,7 +1,25 @@
 const index = require('./');
 
 new index({
-	shouldSplit: true
+	shouldSplit: true,
+	urls: [
+		'https://faq.soylent.com/hc/en-us/articles/212831963-How-Soylent-makes-a-difference',
+		'https://faq.soylent.com/hc/en-us/articles/212767043-What-is-Soylent-',
+		'https://faq.soylent.com/hc/en-us/articles/212769443-Why-Soy-Protein-',
+		'https://faq.soylent.com/hc/en-us/articles/212769723-Expiration-and-shelf-life',
+		'https://faq.soylent.com/hc/en-us/articles/200332079-Can-I-lose-weight-on-Soylent-',
+		'https://faq.soylent.com/hc/en-us/articles/204409635-Preparing-Soylent-Powder-with-the-legacy-measuring-scoop'
+	],
+	tags: [
+		'p',
+		'h1',
+		'h2',
+		'h3',
+		'h4',
+		'h5',
+		'h6'
+	],
+	save: false
 }).then((data) => {
 	console.log('we are done!!!\n\n', data);
 });
